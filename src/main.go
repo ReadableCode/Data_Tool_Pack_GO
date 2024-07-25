8d2b3bc676a2021ff038ae6eb5ea9116f76828ea
jason <jasonwchristiansen@gmail.com>
2024-07-25 10:47:01 -0500

    remove unneeded nesting

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/joho/godotenv"
 
-	// import from utils/google_toools.go
 	"data_tool_pack_go/src/utils"
 )
 
@@ -18,49 +17,49 @@ func main() {
         log.Fatalf("Error loading .env file")
     }
 
-    {
-        // Define the spreadsheet ID, sheet name, and range
-        spreadsheetId := "1pvmIGeanVd0mjIO4-y53OY-z-ueLIY1AF7e-KZGAMzI"
-        sheetName := "rust_test"
-        readRange := "A1:D"
+	// Declare variables
+    var (
+        data          [][]interface{}
+        spreadsheetId string
+        sheetName     string
+        readRange     string
+    )
 
-        // Call the function to read data from the Google Sheet
-        data, err := utils.ReadGoogleSheet(spreadsheetId, sheetName, readRange)
-        if err != nil {
-            log.Fatalf("Unable to retrieve data from sheet: %v", err)
-        }
+	// Call the function to read data from the Google Sheet
+    spreadsheetId = "1pvmIGeanVd0mjIO4-y53OY-z-ueLIY1AF7e-KZGAMzI"
+    sheetName = "rust_test"
+    readRange = "A1:D"
+    data, err = utils.ReadGoogleSheet(spreadsheetId, sheetName, readRange)
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
 
-        // Print the data
-        if len(data) == 0 {
-            fmt.Println("No data found.")
-        } else {
-            fmt.Println("Data:")
-            for _, row := range data {
-                fmt.Println(row)
-            }
-        }
-    }
+	// Print the data
+	if len(data) == 0 {
+		fmt.Println("No data found.")
+	} else {
+		fmt.Println("Data:")
+		for _, row := range data {
+			fmt.Println(row)
+		}
+	}
 
-    {
-        // Define the spreadsheet ID, sheet name, and range
-        spreadsheetId := "1pvmIGeanVd0mjIO4-y53OY-z-ueLIY1AF7e-KZGAMzI"
-        sheetName := "rust_test"
-        readRange := "A1:C"
+	// Call the function to read data from the Google Sheet
+    spreadsheetId = "1pvmIGeanVd0mjIO4-y53OY-z-ueLIY1AF7e-KZGAMzI"
+    sheetName = "rust_test"
+    readRange = "A1:C"
+    data, err = utils.ReadGoogleSheet(spreadsheetId, sheetName, readRange)
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
 
-        // Call the function to read data from the Google Sheet
-        data, err := utils.ReadGoogleSheet(spreadsheetId, sheetName, readRange)
-        if err != nil {
-            log.Fatalf("Unable to retrieve data from sheet: %v", err)
-        }
-
-        // Print the data
-        if len(data) == 0 {
-            fmt.Println("No data found.")
-        } else {
-            fmt.Println("Data:")
-            for _, row := range data {
-                fmt.Println(row)
-            }
-        }
-    }
+	// Print the data
+	if len(data) == 0 {
+		fmt.Println("No data found.")
+	} else {
+		fmt.Println("Data:")
+		for _, row := range data {
+			fmt.Println(row)
+		}
+	}
 }
